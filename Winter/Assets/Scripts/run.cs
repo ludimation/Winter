a362faec66a898d1c7af5f5f39974c3c84fbd142
Ryan Gimson <gimsor@rpi.edu>
2013-11-04 00:32:54 -0500

    Added scripts for wolf movement

---
+++ Winter/Assets/Scripts/run.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class run : MonoBehaviour {
+	
+	private NavMeshAgent agent;
+	
+	void Start () {
+		agent = gameObject.GetComponent<NavMeshAgent>();
+		agent.speed = 4;
+		Vector3 movVec = new Vector3(230,0,460);
+		agent.SetDestination(movVec);
+		
+	}
+	
+	void Update () {
+		
+	}
+}
