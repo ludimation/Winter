be8ec65bda3ca87ce2e82af0111ed9bb44783aad
Andrew Morris <jump3r@dggaming.net>
2013-11-04 18:43:53 -0500

    audio.unity - wind/script edits

--- Winter/Assets/PlayAmbientSound.cs
+++ Winter/Assets/PlayAmbientSound.cs
@@ -4,8 +4,9 @@ using System.Collections;
 public class PlayAmbientSound : MonoBehaviour {
 	
 	public float playChance = 0.01f;
+	public AudioSource source;
 	public AudioClip[] soundClip;
-	public GameObject lister;
+	public GameObject listener;
 
 	// Use this for initialization
 	void Start () {
@@ -15,14 +16,16 @@ public class PlayAmbientSound : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-		float distance = Vector3.Distance(lister.transform.position, transform.position);
+		float distance = Vector3.Distance(listener.transform.position, transform.position);
 		
 		if (Random.value < playChance && distance < 35) {
 			// Play your sound.
 			int clip = (int)Random.Range(0.0f, 2.9f);
-			Debug.Log("Sound Played");
+			if (!AudioSource.isPlaying)  {
+				Debug.Log("Sound Played");
 			Debug.Log(clip);
-			AudioSource.PlayClipAtPoint(soundClip[clip], transform.position);
+				AudioSource.PlayClipAtPoint(soundClip[clip], transform.position);
+			}
 		}
 	}
 }
