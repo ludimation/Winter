bf6541751e788b7d52093d26dc6be05f22235b66
Ryan Gimson <gimsor@rpi.edu>
2013-11-04 17:40:32 -0500

    Added basic wolf script including managing temperature based on player distance

---
+++ Winter/Assets/Scripts/wolf.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System;
+
+public class wolf : MonoBehaviour {
+	
+	//Private member variables
+	private float distanceFromPlayer;
+	private float velocity;
+	private float temperature;
+	
+	//Public member variables
+	public Transform playerTransform;
+	public float maxVelocity;
+	public float maxTemp;
+	public float warmingDistance;
+	public float warmingSpeed;
+		
+	void Start () {
+		//Fill initial state values
+		distanceFromPlayer = PlayerDistance(playerTransform.position);
+		velocity = 0;
+	}
+	
+	
+	void Update () {
+		
+		//Find distance from player
+		distanceFromPlayer = PlayerDistance(playerTransform.position);
+		
+		//Check bounds for Velocity
+		if(velocity > maxVelocity) {
+			velocity = maxVelocity;
+		}
+		if(velocity < 0) {
+			velocity = 0;
+		}
+		
+		//Adjust temperature based on player distance
+		temperature +=  (warmingDistance - distanceFromPlayer) * Time.deltaTime * warmingSpeed;
+		
+		//Check bounds on Temperature
+		if(temperature > maxTemp) {
+			temperature = maxTemp;
+		}
+		if(temperature < 0) {
+			temperature = 0;
+		}
+		
+	}
+	
+	int PlayerDistance(Vector3 player_position) {
+		float x = player_position.x;
+		float y = player_position.y;
+		float z = player_position.z;
+		
+		float distance = Mathf.Sqrt((x * x) + (y * y) + (z * z));
+		return (int) distance;
+	}
+}
