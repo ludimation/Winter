26c5e47a8ffcb0f9b4e42e2872f5bd505df67b8d
Andrew Morris <jump3r@dggaming.net>
2013-11-06 10:49:03 -0500

    added camera lerp to end scene

--- Winter/Assets/FloatController.cs
+++ Winter/Assets/FloatController.cs
@@ -10,9 +10,12 @@ public class FloatController : MonoBehaviour {
 	
 	public GameObject main_camera;
 	public GameObject camera_controller;
+	public float time_before_win = 5f;
 	
 	
 	private bool floating;
+	private bool lerping;
+	private float lerp_speed = 1.0f;
 	// Use this for initialization
 	void Start () {
 	
@@ -23,6 +26,13 @@ public class FloatController : MonoBehaviour {
 		if (floating) {
 			dead_kid.transform.position += Vector3.up * Time.deltaTime * 3.0f;
 			dead_wolf.transform.position += Vector3.up * Time.deltaTime * 3.0f;
+		}
+		
+		if (lerping) {
+			Vector3 dir = main_camera.transform.position - dead_kid2.transform.position;
+			dir = dir.normalized;
+			main_camera.transform.Translate(dir * lerp_speed * Time.deltaTime);
+			main_camera.transform.LookAt(dead_kid2.transform);
 		}
 	}
 	
@@ -37,6 +47,8 @@ public class FloatController : MonoBehaviour {
 		main_camera.GetComponent<MouseOrbit>().enabled = false;
 		Debug.Log("Camera disabled.");
 		
-		camera_controller.transform.LookAt(dead_kid2.transform);
+		main_camera.transform.LookAt(dead_kid2.transform);
+		lerping = true;
 	}
+	
 }
