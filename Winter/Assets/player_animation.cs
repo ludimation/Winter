478bf2026be729765e48ebc10a74b1f872ccef02
Polytonic <kfung@tinycranes.com>
2013-11-06 04:44:38 -0500

    Update Wolf Animations

---
+++ Winter/Assets/player_animation.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class player_animation : MonoBehaviour {
+	
+	private float velocity = 0;
+	
+	// Use this for initialization
+	void Start () {
+		
+		velocity = 1;
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		print ("Hello");
+		//Component worker = gameObject.GetComponent("construction_worker");
+		//print(worker.moveSpeed);
+	}
+}
