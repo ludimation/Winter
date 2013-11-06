4af2558e7b1279645de977c1bc199be168e524b5
Andrew Morris <jump3r@dggaming.net>
2013-11-06 02:35:26 -0500

    Added footsteps, snow scripts, menu screen

---
+++ Winter/Assets/InstructionsScene.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstructionsScene : MonoBehaviour {
+	public Texture2D instructions;
+	
+	void OnGUI () {
+		GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), instructions);
+	}
+	
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.anyKey) {
+			Application.LoadLevel("forest_scene");	
+		}
+	}
+}
