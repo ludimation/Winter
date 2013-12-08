c283ac4137ae42ee5d837abd60b53f35027b5077
Polytonic <kfung@tinycranes.com>
2013-12-07 21:27:49 -0500

    Overhaul Wolf Navigation Script

--- Winter/Assets/Scripts/wolf.cs
+++ Winter/Assets/Scripts/wolf.cs
@@ -3,235 +3,204 @@ using System;
 using System.Diagnostics;
 
 public class wolf : MonoBehaviour {
-	
-	//Private member variables
-	private float velocity;
-	private float temperature  = 0;
-	private bool gameOver;
-	private NavMeshAgent agent;
-	private float sniffCooldown;
-	private bool movingToEnd;
-	private bool started = false;
-	private NavMeshPath path;
-	private float x_prev;
-	private float y_prev;
-	private float z_prev;
-	
-	Stopwatch timer = new Stopwatch();
-	Stopwatch stopped = new Stopwatch();
-	private double tolerance = 1; // Location Tolerance
-	private float stuck = 0; // Stuck Counter
-	
-	enum CharacterState {
-		Sniffing = 0,
-		Walking = 1,
-		Floating = 2,
-	}
-	
-	private CharacterState charState;
-	
-	//Public member variables
-	public Transform playerTransform;
-	public float maxVelocity = 15f;
-	public float maxTemp = 100f;
-	public float warmingDistance = 20f;
-	public float warmingSpeed = 0.1f;
-	public float maxCoolingDistance = 35f;
-	public GameObject girlCorpse;
-	public GameObject wolfCorpse;
-	
-		
-	void Start () {
-		
-		UnityEngine.Random.seed = DateTime.Now.Millisecond;
-		x_prev = gameObject.transform.position.x;
-		y_prev = gameObject.transform.position.y;
-		z_prev = gameObject.transform.position.z;
-		//Fill initial state values
-		velocity = 0;
-		charState = CharacterState.Walking;
-		gameOver = false;
-		agent = GetComponent<NavMeshAgent>();
-		sniffCooldown = 5f;
-		
-		Vector3 movVec = new Vector3(
-	    	UnityEngine.Random.Range(-100,500),
-			0,
-			UnityEngine.Random.Range(213,840));
-		agent.SetDestination(movVec);
-		
-		movingToEnd = false;
-		timer.Start();
-		wolfCorpse.SetActive(false);
-		girlCorpse.SetActive(false);
-	}
-	
-	
-	void Update () {
-		float tempDistance;     //Player distance from wolf, but maxes out at maxCoolingDistance
-		UnityEngine.Random.seed = DateTime.Now.Millisecond;
 
-		if (timer.Elapsed.Seconds >= 3)
-		{
-			x_prev = gameObject.transform.position.x;
-			y_prev = gameObject.transform.position.y;
-			z_prev = gameObject.transform.position.z;	
-			timer.Reset();
-			timer.Start();
-		}
-		
-		if (Math.Abs(x_prev - gameObject.transform.position.x) <= tolerance
-		&&  Math.Abs(z_prev - gameObject.transform.position.z) <= tolerance)
-		{
-			if(started == true)
-				if(charState != CharacterState.Sniffing)
-					if(stopped.IsRunning == false)		
-						stopped.Start();
-				
-		}
-		else
-		{
-			if (stopped.IsRunning == true)
-			{
-				stopped.Reset();
-			}
-		}
-		
-		if (stopped.IsRunning == true)
-		{
-			if (stopped.Elapsed.Seconds >= 3)
-			{
-				print("I am probably stuck!  Generating new coordinates...");
-				Vector3 newLocation = new Vector3(UnityEngine.Random.Range(180f,725f),100,UnityEngine.Random.Range(213f,840f));
-				agent.SetDestination(newLocation);
-				stopped.Reset();
-				movingToEnd = false;
-			}
-		}
-		
-		//Find distance from player
-		tempDistance = PlayerDistance(playerTransform.position);
-		if(tempDistance > maxCoolingDistance) {
-			tempDistance = maxCoolingDistance;
-		}
-	
-		
-		if (movingToEnd == false) {
-			
-			//Adjust temperature based on player distance
-			temperature +=  (warmingDistance - tempDistance) * Time.deltaTime * warmingSpeed;
-			
-			
-			//Check bounds on Temperature
-			if(temperature > maxTemp) {
-				temperature = maxTemp;
-			}
-			if(temperature < 0) {
-				temperature = 0;
-			}
-			if (temperature > 10)
-			{
-				started = true;	
-			}
-			
-			
-			//Manage Velocity
-			if(charState == CharacterState.Walking && temperature > 2) {
-				velocity = .7f * maxVelocity * temperature / maxTemp + .3f * maxVelocity;
-			}
-			else {
-				velocity = 0;	
-			}
-			
-			agent.speed = velocity;
-			
-			if(PlayerDistance(agent.destination) <= .05 && charState != CharacterState.Sniffing) {
-				sniffCooldown = 0;	
-			}
-			
-			//Manage Sniffing
-			if(sniffCooldown <= 0) {
-				if(charState != CharacterState.Sniffing) {
-					charState = CharacterState.Sniffing;
-				}
-				else {
-					sniffCooldown -= Time.deltaTime;
-					if(sniffCooldown <= -5) {
-						charState = CharacterState.Walking;
-						sniffCooldown = UnityEngine.Random.Range(-10f,10f) + 10 + 15 * (temperature / maxTemp);
-						
-						Vector3 newLocation = new Vector3(UnityEngine.Random.Range(180f,725f),100,UnityEngine.Random.Range(213f,840f));
-						agent.SetDestination(newLocation);
-						
-					}
-				}
-			}
-			
-			
-			if(temperature > 2) {
-				sniffCooldown -= Time.deltaTime;
-			}
-			
-			if(temperature == 100f) {
-				movingToEnd = true;
-				charState = CharacterState.Walking;
-				Vector3 newLocation = new Vector3(552f,1f,527f);
-				agent.SetDestination (newLocation);
-				wolfCorpse.SetActive(true);
-				girlCorpse.SetActive(true);
-		
-			}
-		}
-		else {
-			if ( PlayerDistance(agent.destination) 		 <= 3) {
-				velocity = 0f;
-			}
-			if (PlayerDistance(playerTransform.position) <= 3
-			&&	PlayerDistance(agent.destination) 		 <= 3)
-			{
-				charState = CharacterState.Floating;
-				stopped.Reset();
-				stopped.Start();
-	
-				if (stopped.Elapsed.Seconds > 10)
-					Application.LoadLevel("Win");
-			}
-			
-		}
-	}
-	
-	// DISTANCE FORM WOLF TO INPUT VECTOR
-	int PlayerDistance(Vector3 player_position) {
-		//Use 3-D Distance formula to find distance to player
-		float x = player_position.x - gameObject.transform.position.x;
-		float y = player_position.y - gameObject.transform.position.y;
-		float z = player_position.z - gameObject.transform.position.z;
-		
-		float distance = Mathf.Sqrt((x * x) + (y * y) + (z * z));
-		return (int) distance;
-	}
-	
-	public float GetTemp () {
-		return temperature;	
-	}
-	
-	public float GetSpeed () {
-		return velocity;	
-	}
-	
-	public bool GetSniffing () {
-		return charState == CharacterState.Sniffing;	
-	}
-	
-	public bool GetStartedFloating () {
-		return charState == CharacterState.Floating;	
-	}
-	
-	public bool GetGameOver () {
-		return gameOver;	
-	}
-	
-	public bool getFrozen () {
-		return temperature == 0;	
-	}
+    enum CharacterState {
+        Sniffing = 0,
+        Walking  = 1,
+        Floating = 2,
+    }
+    public  Transform      player;
+    private CharacterState animator;
+    private NavMeshAgent   navigator;
+    Stopwatch              sniffer;
+    Stopwatch              location;
+    Stopwatch              timer;
+    // Maybe should have an action queue?
+
+    private float temperature;
+    private float velocity;
+    private bool  ready;
+    private bool  ending;
+
+    private Vector3 destination;
+    private Vector3 historical;
+    private float radius;
+    private float proximity;
+
+    public GameObject girlCorpse;
+    public GameObject wolfCorpse;
+
+
+    // Called at Script Initialization
+    void Start() {
+
+        // Setup Character Controller
+        navigator = GetComponent<NavMeshAgent>();
+        animator  = CharacterState.Sniffing;
+        sniffer   = new Stopwatch();
+        location  = new Stopwatch();
+        timer     = new Stopwatch();
+
+        // Set Default Values
+        temperature = 1f;
+        velocity    = 0f;
+        ready       = true;
+        ending      = false;
+
+        radius = 10;
+        location.Start();
+        historical = gameObject.transform.position;
+
+        /*
+        Vector3 movVec = new Vector3(
+            UnityEngine.Random.Range(-100,500),
+            0,
+            UnityEngine.Random.Range(213,840));
+        navigator.SetDestination(movVec);
+        */
+
+        // Generate New Coordinate
+        // Set Velocity Based on Heat
+
+    }
+
+    // Called Once Every Frame
+    void Update() {
+
+        // If Player is Within Radius of Wolf, Increase Temperature
+        proximity = getProximity(player.position);
+        if (proximity <= radius) {
+            float heat = 1 / proximity;
+            if (heat < 0.1) { heat = 0.1f; }
+            if (heat > 0.6) { heat = 0.6f; }
+            if (sniffer.IsRunning == false) {
+                temperature += heat;
+            }
+        }
+        else if (sniffer.IsRunning == false) {
+            temperature -= 0.005f * proximity;
+        }
+        else if (sniffer.IsRunning == true)  {
+            temperature += 0.02f  * proximity;
+        }
+
+        // Set Some Default Assertions Regarding Wolf Temperature
+        if (temperature < 1)   { temperature = 1;   } // Min Temp
+        if (temperature > 100) { temperature = 100;   // Max Temp
+            ending = true;
+        }
+
+        // Wolf is Stationary Under Threshold Temperature
+             if (temperature <= 15) { velocity = 0; }
+        else if (temperature >  15) {
+
+            // Set a New Destination on Ready Event
+            if (ready == true) {
+                print("Setting New Destination");
+                destination = getDestination();
+                ready = false;
+            } else { setVelocity(); }
+
+            // Trigger Sniffing When Close to Destination
+            if (getProximity(destination) <= 2) {
+                animator = CharacterState.Sniffing;
+                velocity = 0f; // Stop Wolf Movement
+                if (sniffer.IsRunning == false) {
+                    print("Starting Timer");
+                    sniffer.Start();
+                    ready = false;
+            }} // Sniffer Check Outside Temperature Conditional
+        }
+
+        // Set New Destination After 5 Seconds of Sniffing
+        if (sniffer.Elapsed.Seconds >= 5) {
+            print("Resetting Timer");
+            sniffer.Reset();
+            ready = true;
+        }
+
+        // Set Character Animator State Variables
+        if      (velocity == 0) { animator = CharacterState.Sniffing; }
+        else if (velocity >  0) { animator = CharacterState.Walking;  }
+
+        // Handle the Ending Sequence
+        if (ending == true) {
+            destination = new Vector3(552f,1f,527f);
+            wolfCorpse.SetActive(true);
+            girlCorpse.SetActive(true);
+
+            if (getProximity(destination) <= 1) {
+                if (timer.IsRunning == false) { timer.Start(); }
+                else if (timer.Elapsed.Seconds >= 5) {
+                    // Let the Player Roam Around then Trigger End Scene
+                    animator = CharacterState.Floating;
+                    gameObject.transform.position = new Vector3(
+                        transform.position.x,
+                        transform.position.y + 1,
+                        transform.position.z);
+                    velocity = 0;
+                }
+                if (timer.Elapsed.Seconds >= 30) {
+                    Application.LoadLevel("Win");
+                }
+            }
+        }
+
+        // Update the Historical Location Along an Interval (AM I STUCK?!)
+        if (location.Elapsed.Seconds >= 2) {
+            // If Distance from Historical Coordinate to Current Coordinate
+            if (getProximity(historical) <= 5)       {
+            if (animator != CharacterState.Sniffing) {
+                print("I am possibly stuck.  Resetting!");
+                destination = getDestination();
+                ready = false;
+            }}
+            else { historical = gameObject.transform.position; }
+            location.Reset();
+            location.Start();
+        }
+
+        // Set Character Controller State Variables
+        navigator.speed = velocity;
+        navigator.SetDestination(destination);
+        temperature -= 0.1f;
+    }
+
+    // Set Velocity as a Function of Temperature and Player Proximity
+    void setVelocity()
+    {
+        velocity = 0.15f * temperature;
+    }
+
+    // Computes the Proximity of a Given Vector from the Wolf
+    float getProximity(Vector3 input) {
+
+        // Compute the Difference in Coordinate Positions
+        float x = input.x - gameObject.transform.position.x;
+        float y = input.y - gameObject.transform.position.y;
+        float z = input.z - gameObject.transform.position.z;
+        y = 0;
+
+        // Return the Proximity Using the Distance Formula
+        return Mathf.Sqrt((x * x) + (y * y) + (z * z));
+    }
+
+    // Create a New Destination Coordinate
+    Vector3 getDestination()
+    {
+        float offset = UnityEngine.Random.Range(-75f, 50f);
+        float x = gameObject.transform.position.x + offset;
+        float z = gameObject.transform.position.z + offset;
+        float y = UnityEngine.Random.Range(-5f, 5f);
+        print(new Vector3(x, y, z));
+        return new Vector3(x, y, z);
+    }
+
+    // Pass Variables to Mekanim Animation Controller
+    public float GetTemp           () { return temperature; }
+    public float GetSpeed          () { return velocity;    }
+    public bool  GetGameOver       () { return false; }
+    public bool  getFrozen         () { return temperature == 0; }
+    public bool GetSniffing        () { return animator == CharacterState.Sniffing; }
+    public bool GetStartedFloating () { return animator == CharacterState.Floating; }
 }
